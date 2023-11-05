e05c9351d1a36a270984f9c7514753b05386e740
Raf Formes <rafformes@gmail.com>
2023-11-05 16:19:54 +0800

    Move debug draw to c++
    Add shortcuts

---
+++ UESandbox/Source/UESandbox/Private/Utils/DebugHelper.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DrawDebugHelpers.h"
+#include "DebugHelper.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+void UDebugHelper::DrawDebugLineCustom(UObject* WorldContextObject, FVector Origin, FVector Direction, float Length, FLinearColor Color,
+                                       float Duration, float Thickness)
+{
+#if ENABLE_DRAW_DEBUG
+	DrawDebugLine(
+		WorldContextObject->GetWorld(), Origin, Origin + Length * Direction.Normalize(),
+		Color.ToFColor(true), false, Duration, SDPG_World, Thickness);
+#endif
+}
+
+void UDebugHelper::DrawDebugSphereCustom(UObject* WorldContextObject, FVector Center, float Radius, int32 Segments, FLinearColor Color,
+	float Duration, float Thickness)
+{
+#if ENABLE_DRAW_DEBUG
+	DrawDebugSphere(
+		WorldContextObject->GetWorld(), Center, Radius, Segments,
+		Color.ToFColor(true), false, Duration, SDPG_World, Thickness);
+#endif
+}
+
+void UDebugHelper::DrawDirections(UObject* WorldContextObject, FVector Origin, FVector Forward, FVector Up, FVector Right, float Length,
+	float Duration, float Thickness)
+{
+#if ENABLE_DRAW_DEBUG
+	DrawDebugLineCustom(WorldContextObject, Origin, Forward, Length, FColor::Red, Duration, Thickness);
+	DrawDebugLineCustom(WorldContextObject, Origin, Up, Length, FColor::Blue, Duration, Thickness);
+	DrawDebugLineCustom(WorldContextObject, Origin, Right, Length, FColor::Green, Duration, Thickness);
+#endif
+}
+
+void UDebugHelper::DrawDirections2(UObject* WorldContextObject, FVector Origin, FRotator Rotation, float Length, float Duration, float Thickness)
+{
+#if ENABLE_DRAW_DEBUG
+	DrawDirections(
+		WorldContextObject, Origin,
+		UKismetMathLibrary::GetForwardVector(Rotation),
+		UKismetMathLibrary::GetUpVector(Rotation),
+		UKismetMathLibrary::GetRightVector(Rotation),
+		Length, Duration, Thickness);
+#endif
+}
---
+++ UESandbox/Source/UESandbox/Public/Utils/DebugHelper.h
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DebugHelper.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UESANDBOX_API UDebugHelper : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="DebugHelper", meta=(WorldContext="WorldContextObject", DevelopmentOnly))
+	static void DrawDebugLineCustom(
+		UObject* WorldContextObject,
+		FVector Origin,
+		FVector Direction = FVector::ForwardVector,
+		float Length = 100.0,
+		FLinearColor Color = FLinearColor::Red,
+		float Duration = 0.0f,
+		float Thickness = 2.0f);
+	
+	UFUNCTION(BlueprintCallable, Category="DebugHelper", meta=(WorldContext="WorldContextObject", DevelopmentOnly))
+	static void DrawDebugSphereCustom(
+		UObject* WorldContextObject,
+		FVector Center,
+		float Radius = 100.0,
+		int32 Segments = 5,
+		FLinearColor Color = FLinearColor::Red,
+		float Duration = 0.0f,
+		float Thickness = 1.0f);
+
+	UFUNCTION(BlueprintCallable, Category="DebugHelper", meta=(WorldContext="WorldContextObject", DevelopmentOnly))
+	static void DrawDirections(
+		UObject* WorldContextObject,
+		FVector Origin,
+		FVector Forward = FVector::ForwardVector,
+		FVector Up = FVector::UpVector,
+		FVector Right = FVector::RightVector,
+		float Length = 100.0,
+		float Duration = 0.0f,
+		float Thickness = 2.0f);
+
+	UFUNCTION(BlueprintCallable, Category="DebugHelper", meta=(WorldContext="WorldContextObject", DevelopmentOnly))
+	static void DrawDirections2(
+		UObject* WorldContextObject,
+		FVector Origin,
+		FRotator Rotation,
+		float Length = 100.0,
+		float Duration = 0.0f,
+		float Thickness = 2.0f);
+};
